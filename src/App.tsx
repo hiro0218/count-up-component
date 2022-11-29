68b81fdc4549a1de31e24a42071efb790a3f2c2c
hiro <hiro0218@gmail.com>
2022-11-29 09:58:35 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,99 @@
+import './style.css';
+
+import styled from '@emotion/styled';
+import { keyframes, Global } from '@emotion/react';
+import { useEffect, useRef } from 'react';
+
+type Props = {
+  count: number;
+  duration?: number;
+};
+
+const enableRegisterProperty =
+  // @ts-ignore
+  typeof window.CSS.registerProperty !== 'undefined';
+
+const countAnimation = ({ count }: Props) => keyframes`
+  from {
+    --count-number: 0;
+  }
+  to {
+    --count-number: ${count};
+  }
+`;
+
+const CountUpCss = styled.span<Props>`
+  --count-number: ${(props) => props.count};
+
+  animation: ${countAnimation} ${(props) => props.duration ?? 5}s alternate
+    ease-in-out;
+  counter-reset: counter var(--count-number);
+
+  &::after {
+    content: counter(counter);
+  }
+`;
+
+const CountUpJsInnter = styled.span`
+  &::after {
+    content: attr(data-number);
+  }
+`;
+
+const CountUpJs = ({ count, duration = 5 }: Props) => {
+  const ref = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    let timerInterval = -1;
+
+    const timer = () => {
+      if (!ref.current) return;
+
+      const counting = Number(ref.current.dataset.number);
+
+      if (counting < count) {
+        ref.current.dataset.number = String(counting + 1);
+      }
+
+      if (counting >= count) {
+        window.clearInterval(timerInterval);
+      }
+    };
+
+    timerInterval = window.setInterval(
+      timer,
+      Math.trunc((duration * 500) / count),
+    );
+  }, [count, duration]);
+
+  return (
+    <>
+      <CountUpJsInnter ref={ref} data-number="0" aria-hidden="true" />
+      <span className="sr-only">{count}</span>
+    </>
+  );
+};
+
+export default function App() {
+  const count = 5000;
+  const duration = 5;
+
+  return (
+    <>
+      <Global
+        styles={`
+          @property --count-number {
+            syntax: '<integer>';
+            initial-value: 0;
+            inherits: false;
+          }
+        `}
+      />
+      {enableRegisterProperty ? (
+        <CountUpCss count={count} duration={duration} />
+      ) : (
+        <CountUpJs count={count} duration={duration} />
+      )}
+    </>
+  );
+}
